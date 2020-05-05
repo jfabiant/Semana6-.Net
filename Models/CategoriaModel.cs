9c3485b2b7080b2f00eb8b3d6b6b3bcbb6eb6663
John Fabian <john.timoteo@tecsup.edu.pe>
2020-05-04 22:42:33 -0500

    first commit

---
+++ Models/CategoriaModel.cs
@@ -0,0 +1,33 @@
+﻿using Business;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana6_.Net.Models
+{
+    public class CategoriaModel
+    {
+        public ObservableCollection<Categoria> Categorias { get; set; }
+
+        public CategoriaModel()
+        {
+            var lista = (new BCategoria()).Get(0);
+            Categorias = new ObservableCollection<Categoria>(lista);
+        }
+
+        public bool Insertar(Categoria categoria)
+        {
+            return (new BCategoria()).Insert(categoria);
+        }
+
+        public bool Actualizar(Categoria categoria)
+        {
+            return (new BCategoria()).Update(categoria);
+        }
+
+    }
+}
---
+++ ViewModels/ListaCategoriaViewModel.cs
@@ -0,0 +1,63 @@
+﻿using Entity;
+using Semana6_.Net.Models;
+using Semana6_.Net.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Semana6_.Net.ViewModels
+{
+    public class ListaCategoriaViewModel : ViewModelBase
+    {
+
+        public Categoria _SelectedCategoria;
+        public Categoria SelectedCategoria
+        {
+            get { return _SelectedCategoria; }
+            set
+            {
+                if (_SelectedCategoria != value)
+                {
+                    _SelectedCategoria = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+    public ObservableCollection<Categoria> _Categorias;
+        public ObservableCollection<Categoria> Categorias
+        {
+            get { return _Categorias; }
+            set
+            {
+                if (Categorias != value)
+                {
+                    _Categorias = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ICommand NuevoCommand { get; set; }
+        public ICommand ConsultarCommand { get; set; }
+
+        public ListaCategoriaViewModel()
+        {
+            Categorias = new CategoriaModel().Categorias;
+            NuevoCommand = new RelayCommand<Window>(param => Abrir());
+            ConsultarCommand = new RelayCommand<object>(o => { Categorias = (new CategoriaModel()).Categorias; });
+
+        }
+        
+        void Abrir()
+        {
+            ManCategoria window = new ManCategoria(new Categoria());
+            window.Show();
+        }
+    }
+}
